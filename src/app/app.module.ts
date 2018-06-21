0cc61ff0927830c7545532dc09d3c7db0621d577
luisito caiza <luisito.caiza17@hotmail.com>
2018-06-20 22:25:29 -0500

    ajustes css

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,36 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { PrincipalComponent } from './principal/principal.component';
+import {RouterModule, Routes} from '@angular/router';
+import {HttpClient, HttpClientModule} from '@angular/common/http';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 
 // modulo principal
+
+/**Todo: configuracion de rutas**/
+const APP_ROUTES: Routes = [
+    { path: '', component: PrincipalComponent, pathMatch: 'full' },
+    { path: 'principal', component: PrincipalComponent }
+];
+const Routing = RouterModule.forRoot(APP_ROUTES);
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PrincipalComponent
   ],
   imports: [
-    BrowserModule
+      BrowserModule,
+      Routing,//instancia para referenciar las rutas
+      BrowserModule,
+      HttpClientModule,
+      FormsModule,
+      ReactiveFormsModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [
+      HttpClient
+  ],
+  bootstrap: [AppComponent] //npm install bootstrap  -- en angular-cli.json "../node_modules/bootstrap/dist/css/bootstrap.min.css",
 })
 export class AppModule { }
